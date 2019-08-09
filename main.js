cd1dc45cfbb43d10fa7345ef9d221453c73499b0
ravindra yadav <ravindra281298@gmail.com>
2019-08-09 13:31:24 +0530

    updated file

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const bodyParser=require('body-parser');
 const router=express.Router();
 router.use(bodyParser.json());
 
-const url='mongodb://localhost:27017/';
-const dbName='luke-hydra-test-management-dev-beta';
+const url='mongodb://localhost:27017/database.log.2019-08-09T04-28-40';
+const dbName="luke-hydra-test-management-dev-beta";
 const myColl="hierarchicalAnalysis";
 var db=1;
 
@@ -21,16 +21,19 @@ MongoClient.connect(url,(err,client)=>{
 
 
 router.get('/:id',(req,res)=>{
-    if(!req.params.id){
+    if(req.params.id.length<55 || !req.params.id){
         return res.status(400).json({msg:"invalid content :("});
     }
-    console.log('inside the get functions..!!!');
     var id=req.params.id;
+    console.log('inside the get functions..!!!');
+    // res.json({msg:`id is: ${id}`});
+
     operation.show(db,myColl,id,(docs)=>{
-        res.json({msg:`docs:\n ${docs}`});
+        // res.json({msg:`docs:\n ${docs.parent}`});
+        console.log(docs);
+        res.json({msg:'data\n',docs});
         console.log('data displayed..!!');
     });
-    // res.json({msg:`id is: ${id}`});
 });
 
 
--- operation.js
+++ operation.js
@@ -2,8 +2,11 @@ const assert=require('assert');
 
 exports.show=(db,myColl,id,callback)=>{
     const coll=db.collection(myColl);
-    coll.findOne({"_id":id}).topicAnalysis.Chemistry.maxMarks.toArray((err,docs)=>{
+    coll.find({},{projection:{"parent":1,"_id":0}}).toArray((err,docs)=>{
         assert.equal(err,null);
+        // console.log(docs.childCode);
         callback(docs);
     });
 };
+
+// coll.findOne({"_id":id}).topicAnalysis.Chemistry.maxMarks.toArray((err,docs)=>{
