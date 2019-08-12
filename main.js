092b3185f80f7c6aba1f34bb078a97b75b54b42f
ravindra yadav <ravindra281298@gmail.com>
2019-08-12 13:04:10 +0530

    complete Task 1

--- main.js
+++ main.js
@@ -20,35 +20,67 @@ MongoClient.connect(url,(err,client)=>{
 });
 
 var subject=["Chemistry","Maths","Physics"];
+var minMarks=[10000,10000,10000];
+var maxMarks=[-10000,-10000,-10000];
+var avgMarks=[0,0,0];
+var num=-1;
 
-/*
+
+
 router.post('/',(req,res)=>{
     var arr=req.body.id;
-    var minMarks=[],marMarks=[];
+    for(var i=0;i<avgMarks.length;i++)
+    avgMarks[i]=0;
     for(var i=0;i<arr.length;i++){
-        operation.show(db,myColl,arr[i],(docs)=>{
-            console.log(docs);
-            minMarks.push(docs);
-        });   
+        num=0;
+        operation.get_minMarks(db,myColl,arr[i],(docs)=>{
+            for(var j=0;j<subject.length;j++){
+                num=docs[0].topicAnalysis[subject[j]].minMarks;
+                if(num<minMarks[j])
+                minMarks[j]=num;
+                // console.log(num);
+            }
+        });
+        operation.get_maxMarks(db,myColl,arr[i],(docs)=>{
+            for(var j=0;j<subject.length;j++){
+                num=docs[0].topicAnalysis[subject[j]].maxMarks;
+                if(num>maxMarks[j])
+                maxMarks[j]=num;
+                // console.log(num);
+            }
+        });
+        operation.get_avgMarks(db,myColl,arr[i],(docs)=>{
+            for(var j=0;j<subject.length;j++){
+                avgMarks[j]+=((docs[0].topicAnalysis[subject[j]].totalObtainedMarks)/(docs[0].numberOfStudents));
+            }
+            // console.log(docs);
+        });
     }
-    res.json({msg:"done",minMarks});
-})
-*/
+    res.json({msg:'done'});
+});
+
           
-router.get('/:id',(req,res)=>{
-    if(req.params.id.length<55 || !req.params.id){
-        return res.status(400).json({msg:"invalid content :("});
-    }
-    var id=req.params.id;
-    console.log('inside the get functions..!!!');
-    // res.json({msg:`id is: ${id}`});
-    
-    operation.show(db,myColl,id,(docs)=>{
-        // res.json({msg:`docs:\n ${docs.parent}`});
-        console.log(docs[0].topicAnalysis.Chemistry.minMarks);    // to fetch content from array :)
-        res.json({msg:'data\n',docs});
-        console.log('data displayed..!!');
-    });
+router.get('/',(req,res)=>{
+    // console.log(minMarks,maxMarks);
+    res.json({Subjects:`MinMarks MaxMarks AvgMarks`,
+              Chemistry:`${minMarks[0]}      ${maxMarks[0]}     ${avgMarks[0].toFixed(2)}`,
+              Physics:`  ${minMarks[1]}      ${maxMarks[1]}     ${avgMarks[1].toFixed(2)}`,
+              Maths:`    ${minMarks[2]}      ${maxMarks[2]}     ${avgMarks[2].toFixed(2)}`});
 });
 
 module.exports=router;
+
+
+// console.log(docs[0].topicAnalysis["Chemistry"].minMarks);
+            // num=docs[0].topicAnalysis.Chemistry.minMarks;
+            // if(num<minMarks[0]){
+            //     minMarks[0]=num;
+            // }
+            // num=docs[0].topicAnalysis.Physics.minMarks;
+            // if(num<minMarks[1]){
+            //     minMarks[1]=num;
+            // }
+            // num=docs[0].topicAnalysis.Maths.minMarks;
+            // if(num<minMarks[2]){
+            //     minMarks[2]=num;
+            // }
--- operation.js
+++ operation.js
@@ -1,15 +1,25 @@
 const assert=require('assert');
-var num=1;
-exports.show=(db,myColl,id,callback)=>{
+
+exports.get_minMarks=(db,myColl,id,callback)=>{
     const coll=db.collection(myColl);
-    coll.find({"_id":id},{projection:{"topicAnalysis.Chemistry.minMarks":1,"_id":0}}).toArray((err,docs)=>{
+    coll.find({"_id":id},{projection:{"topicAnalysis.Chemistry.minMarks":1,"topicAnalysis.Physics.minMarks":1,"topicAnalysis.Maths.minMarks":1,"_id":0}}).toArray((err,docs)=>{
         assert.equal(err,null);
-        // console.log(docs.childCode);
-        num=docs;
-        callback(num);
+        callback(docs);
     });
 };
 
-// coll.findOne({"_id":id}).topicAnalysis.Chemistry.maxMarks.toArray((err,docs)=>{
+exports.get_maxMarks=(db,myColl,id,callback)=>{
+    const coll=db.collection(myColl);
+    coll.find({"_id":id},{projection:{"topicAnalysis.Chemistry.maxMarks":1,"topicAnalysis.Physics.maxMarks":1,"topicAnalysis.Maths.maxMarks":1,"_id":0}}).toArray((err,docs)=>{
+        assert.equal(err,null);
+        callback(docs);
+    });
+};
 
-//coll.find({"_id":id},{projection:{"topicAnalysis.Chemistry.minMarks":1,"_id":0}}).toArray((err,docs)=>{
+exports.get_avgMarks=(db,myColl,id,callback)=>{
+    const coll=db.collection(myColl);
+    coll.find({"_id":id},{projection:{"topicAnalysis.Chemistry.totalObtainedMarks":1,"topicAnalysis.Physics.totalObtainedMarks":1,"topicAnalysis.Maths.totalObtainedMarks":1,"numberOfStudents":1,"_id":0}}).toArray((err,docs)=>{
+        assert.equal(err,null);
+        callback(docs);    
+    });
+};
